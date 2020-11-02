4714063065e977b48f58d97fc95e752c14a41bf4
YusufNur5 <yusuf.nurmansyah@students.amikom.ac.id>
2020-11-02 21:26:02 +0700

    Belajar List View (Update 2 Nov 2020)

---
+++ app/src/main/java/com/example/pcs_listview_0097/MainActivity2.kt
@@ -0,0 +1,24 @@
+package com.example.pcs_listview_0097
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity2 : AppCompatActivity() {
+    private val movie = arrayOf("Bad Boys for Life", "The Old Guard", "Raised by Wolves", "Elite", "The Walking Dead: World Beyond", "Artemis Fowl", "Black Box", "Riverdale", "Law & Order: Special Victims Unit", "Scary Movie 5", "Star Trek: Discovery", "Hubie Halloween", "District 9", "The Hurricane Heist", "Paddington 2", "Pride & Prejudice")
+    private lateinit var lvMovie: ListView
+    private lateinit var movieAdapter: ArrayAdapter<String>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        movieAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, movie)
+
+        lvMovie = findViewById(R.id.lv_movie)
+        lvMovie.adapter = movieAdapter
+
+    }
+}
